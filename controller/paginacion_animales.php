c0bbc34fe428c4bda1d5412f895c2a6869a99ca3
Marcos Lopez Medina <166430537+Marrkitu2@users.noreply.github.com>
2024-11-15 19:06:31 +0100

    Empezar de 0

--- controller/paginacion_animales.php
+++
@@ -1,9 +0,0 @@
-<?php
-function obtenerPaginacion($total_animales, $animales_por_pagina) {
-    return ceil($total_animales / $animales_por_pagina);
-}
-
-function obtenerOffset($pagina_actual, $animales_por_pagina) {
-    return ($pagina_actual - 1) * $animales_por_pagina;
-}
-?>
